aa7212347d8a03bec3eda3b022daf6e0f6f9b2e3
Hakobune123 <88906379+Hakobune123@users.noreply.github.com>
2023-11-06 09:52:45 -0500

    Update database.php

--- login/Private/database.php
+++ login/Private/database.php
@@ -8,5 +8,11 @@ define('DB_NAME', 'login_db');
 
 if(!$connection = mysqli_connect(DB_HOST,DB_USER,DB_PASS,DB_NAME)){
 
+   // die("Failed to connect");
+}
+
+
+$string = "mysql:host=".DB_HOST."; dbname=".DB_NAME;
+if(!$connection = new PDO($string,DB_USER,DB_PASS)){
     die("Failed to connect");
 }
