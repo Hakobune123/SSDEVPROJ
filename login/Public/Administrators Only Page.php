3098997def85fc37386209c477d8b525902c11dd
Hakobune123 <88906379+Hakobune123@users.noreply.github.com>
2023-11-06 09:55:31 -0500

    Add files via upload

---
+++ login/Public/Administrators Only Page.php
@@ -0,0 +1,22 @@
+<?php
+
+require "../private/autoload.php"
+
+
+?>
+
+
+<! DOCTYPE html>
+<html>
+
+
+<head>
+<title>Home</title>
+</head>
+
+
+
+</style>
+<h1>This is the home page </h1>
+</body>
+</html>
